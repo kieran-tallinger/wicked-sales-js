a5325c87d1bc25405187c6a7a82d3a8c7d5f1f75
Kieran Tallinger <dellingerkieran@gmail.com>
2020-03-09 15:48:33 -0700

    Created a new component call ProductDetails and set its initial state and render

---
+++ client/components/product-details.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+
+export default class ProductDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+  }
+
+  render() {
+    return (
+      null
+    );
+  }
+}
