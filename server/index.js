3eecae7b08682169938441f55591b09c9f06795b
Kieran Tallinger <dellingerkieran@gmail.com>
2020-03-09 17:47:35 -0700

    Added a new GET endpoint to the index.js for requests to api/cart

--- server/index.js
+++ server/index.js
@@ -56,6 +56,9 @@ app.get('/api/products/:productId', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/cart', (req, res, next) => {
+  return res.json();
+});
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
