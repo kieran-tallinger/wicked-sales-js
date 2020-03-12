ba959d176806b658d0f8441e0cb56cde90ff5e7c
Kieran Tallinger <dellingerkieran@gmail.com>
2020-03-11 20:58:58 -0700

    Adjusted the bootstrap styling to display CartSummary as intended

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 export default function CartSummaryItem(item) {
   return (
-    <div>
-      <div className="row card">
-        <div>
-          <img className="card-image" src={item.img} alt={item.short}/>
-        </div>
-        <div>
-          <h5 className="card-title">{item.name}</h5>
-          <p>${item.price}</p>
-          <p className="card-text">{item.short}</p>
-        </div>
+    <div className="row justify-content-center col-12 card mb-2">
+      <div className="col-4">
+        <img className="card-image" src={item.img} alt={item.short}/>
+      </div>
+      <div className="col-8">
+        <h5 className="card-title">{item.name}</h5>
+        <p>${item.price}</p>
+        <p className="card-text">{item.short}</p>
       </div>
     </div>
   );
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -5,15 +5,15 @@ export default function CartSummary(props) {
   let total = 0;
   let body = null;
   if (props.items.length === 0) {
-    body = <div><span>Your Cart is currently empty! Go checkout our catalog!</span></div>;
+    body = <div className="row"><span>Your Cart is currently empty! Go checkout our catalog!</span></div>;
   } else {
-    body = <div>
+    body = <div className="row mb-3">
       {
         props.items.map(product => {
           total = total + product.price;
           return (
             <CartSummaryItem
-              key={product.productId}
+              key={product.cartItemId}
               img={product.image}
               name={product.name}
               price={product.price}
@@ -25,11 +25,11 @@ export default function CartSummary(props) {
     </div>;
   }
   return (
-    <div className="row justify-content-center">
-      <a onClick={() => this.props.setView('catalog', {})} className="my-2 back text-muted">&lt; Back to catalog</a>
-      <h2>My Cart</h2>
+    <div className="container">
+      <a onClick={() => props.setView('catalog', {})} className="row my-3 back text-muted">&lt; Back to catalog</a>
+      <h2 className="row mb-4">My Cart</h2>
       {body}
-      <h3>Item Total:${total}</h3>
+      <h3 className="row pb-3">Item Total:${total}</h3>
     </div>
   );
 }
